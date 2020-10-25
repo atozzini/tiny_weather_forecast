03331104836f8b2ca61551567f027cb95c3fd668
Alexandre Tozzini <alexandre.tozzini.at@gmail.com>
2020-10-25 12:50:24 -0300

    remove itinializer

--- lib/tiny_weather_forecast.rb
+++ lib/tiny_weather_forecast.rb
@@ -2,20 +2,16 @@ require "tiny_weather_forecast/version"
 require 'json'
 
 module TinyWeatherForecast
-  attr_reader :authentication_token, :base_url
-
-  def initialize(authentication_token, city)
-    @authentication_token = authentication_token
-    @city = city
-    @base_url = 'https://api.openweathermap.org/data/2.5/onecall?'
-  end
-
-  def forecast
-    resp = RestClient.get "#{@base_url}lat=#{lat}&lon=#{lon}&appid=#{@authentication_token}"
+  def forecast(authentication_token, city)
+    resp = RestClient.get "#{base_url}lat=#{lat}&lon=#{lon}&appid=#{authentication_token}"
     puts JSON.parse(resp.body)
   end
 
   private
+
+  def base_url
+    'https://api.openweathermap.org/data/2.5/onecall?'
+  end
 
   def location
     Geocoder.search(@city).first.coordinates
